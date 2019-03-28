56456265b3a9bf98b3a54baec2cfb08c2c351ec9
Maria Micek <mmicek16@wp.pl>
2019-03-28 11:35:01 +0100

    added moment to script.js and index.html

--- new-project/script.js
+++ new-project/script.js
@@ -1,7 +1,15 @@
+// const moment = require('moment')     //tylko dla środowiska node, a nie dla przeglądarki (dla przeglądarki trzeba załączyc ścieżkę w scripcie)
+
 console.log('hello world')
 
 for (let i = 0; i < 10; i++) {
     console.log(`Line ${i}`)
 }
 
-console.log(new Date().toString())
+const timeNow = new Date()
+console.log(timeNow.toString())
+
+const momentNow = moment().endOf('day').fromNow()
+console.log(momentNow)
+
+document.getElementById('content').innerHTML = `<span> The end of the day: ${momentNow} </span>`
